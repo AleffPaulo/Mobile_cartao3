8d1d0e8741bf8db7704a86a3bd75877a0b191310
AleffPaulo <aleffpaulosilvanogueira@gmail.com>
2025-10-18 14:08:14 -0300

    ETAPA: Dependencias iniciais, cor e temas

---
+++ app/src/main/java/com/aleff/meucartaodevisitas/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.aleff.meucartaodevisitas.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Cores primárias do tema claro
+val PrimaryLight = Color(0xFF1976D2)  // Azul vibrante
+val SecondaryLight = Color(0xFF424242)  // Cinza escuro
+val TertiaryLight = Color(0xFFFF6F00)  // Laranja para acentos
+
+// Cores primárias do tema escuro
+val PrimaryDark = Color(0xFF90CAF9)  // Azul claro
+val SecondaryDark = Color(0xFFE0E0E0)  // Cinza claro
+val TertiaryDark = Color(0xFFFFB74D)  // Laranja suave
+
+// Cores de fundo
+val BackgroundLight = Color(0xFFFAFAFA)
+val BackgroundDark = Color(0xFF121212)
